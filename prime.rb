0fef21aceea3368a2933d4fe758d3891df4ac811
Joseph Perez <hello@josephdanielperez.com>
2020-07-01 03:20:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,16 @@
 # Add  code here!
-def prime?(array)
-  array.prime?
+def prime?(x)
+  if x == 1
+    return true
+  elsif x < 1
+    return false
+  else
+  for y in 2..(x-1)
+    if (x % y) == 0
+      return false
+    end
+  end
+end
+
+  true
 end
