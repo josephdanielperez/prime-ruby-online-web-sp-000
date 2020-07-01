51077f4a2533c426debe07303122e51ebc4e8f19
Joseph Perez <hello@josephdanielperez.com>
2020-07-01 03:21:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,4 @@
 # Add  code here!
-def prime?(num)
-  return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-  true
-end
+def prime?
+    ('1' * self) !~ /^1?$|^(11+?)\1+$/
+  end
