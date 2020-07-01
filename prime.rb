6e20a0d60a48fffd6023ab45c0e831201f993e4e
Joseph Perez <hello@josephdanielperez.com>
2020-07-01 03:28:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 # Add  code here!
 def prime?(n)
-  return false if n < 2
-
-  (2..n/2).none?{|i| n % i == 0}
+  if n <= 1
+    false
+  elsif n == 2
+    true
+  else 
+    (2..n/2).none? { |i| n % i == 0}
+  end
 end
